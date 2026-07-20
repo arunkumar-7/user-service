6aee7aaa63ce646517ca0b861e0e893d6d093b6c
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-07-20 23:34:37 +0530

    chore: initialize user service project structure

---
+++ src/main/java/com/userservice/entity/User.java
@@ -0,0 +1,9 @@
+package com.userservice.entity;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+}
